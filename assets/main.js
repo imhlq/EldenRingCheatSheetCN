e13dd12d5bf42de282ea7b4280df5751ef50176f
Richard Hou <im_hlq@163.com>
2022-03-14 18:47:34 +0800

    Initial project

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+var profiles = JSON.parse(localStorage.getItem("profiles"))
+if(profiles === null) {
+    profiles = {}
+}
+
+
+jQuery(document).ready(function($) {
+    initializeProfile();
+
+    $('ul li[data-id]').each(function() {
+        addCheckbox(this);
+    });
+
+    $('.checkbox input[type="checkbox"]').click(function() {
+        var id = $(this).attr('id');
+        var isChecked = profiles.checklistData[id] = $(this).prop('checked');
+        if (isChecked === true) {
+          $('[data-id="'+id+'"] label').addClass('completed');
+        } else {
+          $('[data-id="'+id+'"] label').removeClass('completed');
+        }
+        localStorage.setItem("profiles", JSON.stringify(profiles));
+    });
+
+    $('.nav li a').on('click', function(el) {
+        profiles.current_tab = $(this).attr('href');
+        localStorage.setItem("profiles", JSON.stringify(profiles));
+    })
+
+})
+
+function initializeProfile() {
+    if(!('checklistData' in profiles))
+        profiles.checklistData = {};
+    if(!('current_tab' in profiles)){
+        profiles.current_tab = '#tabItems'
+    } else {
+        var sel = $('.nav li a[href="' + profiles.current_tab + '"]');
+        var tab = new bootstrap.Tab(sel);
+        bootstrap.Tab.getInstance(sel).show();
+    }
+        
+}
+
+function addCheckbox(el) {
+    var $el = $(el);
+    var content = $el.html().split('\n')[0];
+    content = 
+        '<div class="checkbox">' +
+            '<label>' +
+                '<input type="checkbox" id="' + $el.attr('data-id') + '">' +
+                '<span class="item_content">' + content + '</span>' +
+            '</label>' +
+        '</div>';
+    $el.html(content).append($el.children('ul'));
+
+    if (profiles.checklistData[$el.attr('data-id')]===true) {
+        $('#' + $el.attr('data-id')).prop('checked', true);
+        $('label', $el).addClass('completed');
+    }
+}
+
+function clear() {
+    localStorage.clear();
+}
