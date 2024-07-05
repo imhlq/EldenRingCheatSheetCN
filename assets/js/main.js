1dabc1785bfb051fa40e9f99bd8e24403039a581
Richard <im_hlq@163.com>
2024-07-05 12:16:10 -0400

    theme setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,260 +1,87 @@
+import { readFile, getNames, fetchInventory, findItemQuantities } from "./save.js";
+
 var profiles = JSON.parse(localStorage.getItem("profiles"));
 if (profiles === null) {
   profiles = {};
 }
+var file_read = null;
+var slots = [];
+var selected_slot;
+var item_list = [];
 
-jQuery(document).ready(function ($) {
+jQuery(document).ready(async function ($) {
   initializeProfile();
 
-  $("ul li[data-id]").each(function () {
-    addCheckbox(this);
+  // Load Save Script
+  $("#saveLoadBtn").click(function () {
+    $("#fileSelector").click();
   });
-
-  $('.checkbox input[type="checkbox"]').click(function () {
-    var id = $(this).attr("data-id");
-    var isChecked = (profiles.checklistData[id] = $(this).prop("checked"));
-    if (isChecked === true) {
-      $('[data-id="' + id + '"] label').addClass("completed");
-    } else {
-      $('[data-id="' + id + '"] label').removeClass("completed");
+  $("#fileSelector").change(async function (evt) {
+    var file = evt.target.files[0];
+    if (file) {
+      file_read = await readFile(file);
+      var chr_names = getNames(file_read);
+      updateSlotDropdown(chr_names);
+      $("#slot_selector").change(function(e) {
+        // $("#calculate").css("display", "inline-block");
+        calculateSave();
+      });
     }
-    localStorage.setItem("profiles", JSON.stringify(profiles));
-    flesh_hide_completed();
-  });
-
-  // List Checkbox
-  $(".nav li a").on("click", function (el) {
-    profiles.current_tab = $(this).attr("href");
-    localStorage.setItem("profiles", JSON.stringify(profiles));
-  });
-
-  // Filter Buttons
-  $('.btn-group input[type="checkbox"]').click(function () {
-    var is_hidden = $(this).is(":checked");
-    var item_toggle = $(this).attr("data-bs-toggle");
-    $("li." + item_toggle).each(function () {
-      if (!is_hidden) {
-        $(this).css("display", "none");
-      } else {
-        $(this).css("display", "");
-      }
-    });
-  });
-
-  flesh_hide_completed();
-
-  $("#hide_complete").click(function () {
-    profiles.is_hide_complete = $(this).is(":checked");
-    flesh_hide_completed();
-  });
-
-  $("#reset_storage").click(function () {
-    clear();
-  });
-
-  var jets = [
-    new Jets({
-      searchTag: "#walkthrough_search",
-      contentTag: "#walkthrough_list ul",
-    }),
-    new Jets({
-      searchTag: "#boss_search",
-      contentTag: "#boss_list ul",
-    }),
-    new Jets({
-      searchTag: "#armor_search",
-      contentTag: "#armor_list ul",
-    }),
-    new Jets({
-      searchTag: "#weapon_search",
-      contentTag: "#weapon_list ul",
-    }),
-  ];
-
-  $(".lang-en").click(function () {
-    profiles.lang = "engus";
-    localStorage.setItem("profiles", JSON.stringify(profiles));
-    changeLang(profiles.lang);
-    jets.forEach(jet => {
-        jet.update(true);
-    })
-  });
-  $(".lang-si").click(function () {
-    profiles.lang = "zhocn";
-    localStorage.setItem("profiles", JSON.stringify(profiles));
-    changeLang(profiles.lang);
-    jets.forEach(jet => {
-        jet.update(true);
-    })
-  });
-  $(".lang-tr").click(function () {
-    profiles.lang = "zhotw";
-    localStorage.setItem("profiles", JSON.stringify(profiles));
-    changeLang(profiles.lang);
-    jets.forEach(jet => {
-        jet.update(true);
-    })
   });
 
 
-
-  $("#walkthrough_search").keyup(function () {
-    $("#walkthrough_list").unhighlight();
-    $("#walkthrough_list").highlight($(this).val());
-  });
-
-  $("#boss_search").keyup(function () {
-    $("#boss_list").unhighlight();
-    $("#boss_list").highlight($(this).val());
-  });
-
-  $("#armor_search").keyup(function () {
-    $("#armor_list").unhighlight();
-    $("#armor_list").highlight($(this).val());
-  });
-
-  $("#weapon_search").keyup(function () {
-    $("#weapon_list").unhighlight();
-    $("#weapon_list").highlight($(this).val());
-  });
 });
-
-function exportStorage() {
-  var eleLink = document.createElement("a");
-  eleLink.download = "您的缓存.txt";
-  eleLink.style.display = "none";
-  var blob = new Blob([localStorage.getItem("profiles")]);
-  eleLink.href = URL.createObjectURL(blob);
-  document.body.appendChild(eleLink);
-  eleLink.click();
-  document.body.removeChild(eleLink);
-}
-
-function importStorage() {
-  var temp = prompt(
-    "请把您的缓存整个粘贴进来",
-    "输入奇怪的东西可能导致本地缓存丢失，若有问题请重置（删除本段文字）"
-  );
-  localStorage.clear();
-  profiles = JSON.parse(temp);
-  localStorage.setItem("profiles", JSON.stringify(profiles));
-  location.reload();
-}
 
 function initializeProfile() {
   if (!("checklistData" in profiles)) profiles.checklistData = {};
   if (!("current_tab" in profiles)) {
     profiles.current_tab = "#tabItems";
   } else {
-    $('[href="' + profiles.current_tab + '"]').tab("show");
+    // TBD
   }
   if (!("is_hide_complete" in profiles)) {
     profiles.is_hide_complete = true;
   } else {
     $("#hide_complete").prop("checked", profiles.is_hide_complete);
   }
-  if (!("lang" in profiles)) {
-    profiles.lang = "zhocn";
-  } else {
-    changeLang(profiles.lang);
-  }
 }
 
-function changeLang(lang) {
-  if (lang === "zhocn") {
-    $(".lang-si").prop("checked", true);
-    $("html").attr("lang", "zh-Hans");
-  } else if (lang === "engus") {
-    $(".lang-en").prop("checked", true);
-    $("html").attr("lang", "en");
-  } else if (lang === "zhotw") {
-    $(".lang-tr").prop("checked", true);
-    $("html").attr("lang", "zh-Hant");
-  }
-  switchTranslation(lang);
-}
+function updateSlotDropdown(slot_name_list) {
+    const select = $("#slot_select");
+    select.html(`
+        <select aria-label="Select slot" id="slot_selector">
+            <option hidden selected>选择存档角色</option>
+        </select>
+        <label>角色栏</label>
+        <i>arrow_drop_down</i>`);
 
-function switchTranslation(lang) {
-  $.getJSON("assets/lang_" + lang + ".json", function (data) {
-    $('input[type="checkbox"').each(function () {
-      var checkboxId = $(this).attr("data-id");
-      var itemContent = $(this).siblings(".item_content");
-      if (data[lang][checkboxId]) {
-        itemContent.text(data[lang][checkboxId]);
-      }
-    });
-    
-  });
-}
-
-function addCheckbox(el) {
-  var $el = $(el);
-  var content = $el.html().split("\n")[0];
-  var dataId = $el.attr("data-id");
-  prefix = dataId.split("_")[0];
-  var tooltip_text = ""
-  if (prefix !== "is" && prefix !== "w" && prefix !== "b") {
-    tooltip_text = "Item ID: " + dataId.split("_")[1]
-  };
-
-  content =
-    '<div class="checkbox">' +
-    "<label title='" + tooltip_text + "'>" +
-    '<input type="checkbox" data-id="' + dataId + '">' +
-    '<span class="item_content">' + content + "</span>" +
-    "</label>" +
-    "</div>";
-  $el.html(content).append($el.children("ul"));
-
-  if (profiles.checklistData[dataId] === true) {
-    $("#" + dataId).prop("checked", true);
-    $("label", $el).addClass("completed");
-  }
-
-  // Add hover event to show tooltips
-  $("label", $el).hover(
-    function () {
-      var tooltip = $(this).attr("title");
-      $('<div class="tooltip">' + tooltip + '</div>')
-        .appendTo('body')
-        .fadeIn('slow');
-    }, function () {
-      $('.tooltip').remove();
+    const selector = select.find("select");
+    for (let i = 0; i < 10; i++) {
+        if (slot_name_list[i] === "") {
+            selector.append(`<option value="${i}" disabled> - </option>`);
+        } else {
+            selector.append(`<option value="${i}"> ${slot_name_list[i]} </option>`);
+        }
     }
-  ).mousemove(function (e) {
-    var mousex = e.pageX + 20; // Get X coordinates
-    var mousey = e.pageY + 10; // Get Y coordinates
-    $('.tooltip')
-      .css({ top: mousey, left: mousex });
-  });
+    select.show();
 }
 
-function clear() {
-  localStorage.clear();
-}
+function calculateSave() {
+  selected_slot = $("#slot_selector option:selected").val();
+  item_list, slots = fetchInventory(file_read, selected_slot);
+  const itemsQuantities = findItemQuantities(slots[selected_slot]);
+  let globalCounter = 0;
+  const itemsFound = itemsQuantities.reduce((prev, cur) => prev + cur, 0);
+  globalCounter += itemsFound;
 
-function category_toggle() {
-  var is_hidden = $(this).is(":checked");
-  var item_toggle = $(this).cloest(".btn-group").find("[data-item-toggle]");
-  if (is_hidden) {
-    item_toggle
-      .not(function () {
-        return this.checked === is_hidden;
-      })
-      .click();
-  }
-}
+  console.log(itemsQuantities);
+  console.log(globalCounter);
 
-function flesh_hide_completed() {
-  var is_hidden = profiles.is_hide_complete;
-  // $('#hide_complete').is(':checked');
-  $("li .completed").each(function () {
-    if (!is_hidden) {
-      $(this).css("display", "none");
-    } else {
-      $(this).css("display", "");
-    }
-  });
-  profiles.is_hide_complete = is_hidden;
-  localStorage.setItem("profiles", JSON.stringify(profiles));
+
+  // let globalTotal = 0;
+  // const totalItems = quantifiableItems.reduce((prev, cur) => prev + cur.places.length, 0);
+  // globalTotal += totalItems;
+
+  // Update the progress
+  // $("#progress").text(`${globalCounter}/${globalTotal}`);
 }
